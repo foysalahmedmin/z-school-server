fe6eb67e8ff86e248d10e8b0fa53d8c21acdb800
foysalahmedmin <foysalahmedmin@gmail.com>
2023-11-30 13:45:04 +0600

    schema update

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -1,50 +1,57 @@
 import { Schema, model } from 'mongoose';
 import { Address, Guardian, Name, Student } from './student.interface';
 
-const NameSchema = new Schema<Name>({
-  first_name: { type: String, required: true },
+const nameSchema = new Schema<Name>({
+  first_name: { type: String, required: [true, 'First name is required'] },
   middle_name: { type: String },
   last_name: { type: String },
 });
 
-const AddressSchema = new Schema<Address>({
+const addressSchema = new Schema<Address>({
   present: { type: String, required: true },
   permanent: { type: String, required: true },
 });
 
-const GuardianSchema = new Schema<Guardian>({
+const guardianSchema = new Schema<Guardian>({
   name: { type: String, required: true },
   relation: { type: String, required: true },
   occupation: { type: String, required: true },
   contact_number: { type: String, required: true },
-  address: AddressSchema,
+  address: addressSchema,
 });
 
 const StudentSchema = new Schema<Student>({
-  id: { type: String, required: true },
+  id: { type: String, required: true, unique: true },
   name: {
-    type: NameSchema,
+    type: nameSchema,
     required: true,
   },
   gender: {
     type: String,
-    enum: ['male', 'female'],
+    enum: {
+      values: ['male', 'female', 'others'],
+      message:
+        "The gender field can only one of the following: 'male', 'female' or 'others'",
+    },
   },
   date_of_birth: { type: String },
-  email: { type: String, required: true },
-  contact_number: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  contact_number: { type: String, required: true, unique: true },
   emergency_contact_number: { type: String, required: true },
   blood_group: {
     type: String,
-    enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+    enum: {
+      values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+      message: '{VALUE} is not valid',
+    },
   },
   address: {
-    type: AddressSchema,
+    type: addressSchema,
     required: true,
   },
-  guardian: { type: [GuardianSchema], required: true },
-  local_guardian: { type: GuardianSchema, required: true },
-  is_active: { type: Boolean, required: true },
+  guardian: { type: [guardianSchema], required: true },
+  local_guardian: { type: guardianSchema, required: true },
+  is_active: { type: Boolean, default: true, required: true },
 });
 
 export const StudentModel = model<Student>('Student', StudentSchema);
