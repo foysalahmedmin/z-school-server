24ab0b6675ec3e16a9a4f39cb9844cd5732151cb
foysalahmedmin <foysalahmedmin@gmail.com>
2023-12-20 23:39:32 +0600

    data query

---
+++ src/app/modules/student/student.constant.ts
@@ -0,0 +1,8 @@
+export const studentSearchableFields = [
+  'email',
+  'name.first_name',
+  'address.present.country',
+  'address.present.street',
+  'address.present.city',
+  'address.present.details',
+];
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,20 +1,33 @@
 import httpStatus from 'http-status';
 import mongoose from 'mongoose';
+import QueryBuilder from '../../builder/QueryBuilder';
 import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
+import { studentSearchableFields } from './student.constant';
 import { TStudent } from './student.interface';
 import { Student } from './student.model';
 import { studentUpdateDataModifier } from './student.utils';
 
-const getAllStudentsFromDB = async () => {
-  const result = await Student.find()
-    .populate('admission_semester')
-    .populate({
-      path: 'academic_department',
-      populate: {
-        path: 'faculty',
-      },
-    });
+const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
+  const studentQuery = new QueryBuilder(
+    Student.find()
+      .populate('user')
+      .populate('admission_semester')
+      .populate({
+        path: 'academic_department',
+        populate: {
+          path: 'faculty',
+        },
+      }),
+    query,
+  )
+    .search(studentSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await studentQuery.modelQuery;
   return result;
 };
 
